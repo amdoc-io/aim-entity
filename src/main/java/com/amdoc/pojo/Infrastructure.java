8a8810d366372761a24c1fbff8401cabdb00e5d0
Tung Dinh <tungxd301@gmail.com>
2024-07-09 14:02:58 -0700

    Changes

--- src/main/java/com/amdoc/pojo/Infrastructure.java
+++ src/main/java/com/amdoc/pojo/Infrastructure.java
@@ -2,6 +2,7 @@ package com.amdoc.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
+import lombok.NonNull;
 import lombok.Value;
 import lombok.experimental.NonFinal;
 import lombok.extern.jackson.Jacksonized;
@@ -16,7 +17,9 @@ import java.time.ZonedDateTime;
 public class Infrastructure {
 
     String id;
+    @NonNull
     String email;
+    @NonNull
     String organizationId;
     String gitProvider;
     GitOAuthToken gitOauthToken;
